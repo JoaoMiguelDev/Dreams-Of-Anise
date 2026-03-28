22b5c0147b3f1766b91a76e730d231a952e1c3b6
JoaoMiguelDev <jmsabarreto67@gmail.com>
2026-03-28 18:27:10 -0300

    Level 4 e 5

---
+++ Scripts/Levels/Level4.cs
@@ -0,0 +1,8 @@
+using Godot;
+using System;
+
+public partial class Level4 : Level
+{
+    protected override int LevelId => 4;
+    protected override int NumberOfActions => 36;	
+}
---
+++ Scripts/Levels/Level5.cs
@@ -0,0 +1,8 @@
+using Godot;
+using System;
+
+public partial class Level5 : Level
+{
+    protected override int LevelId => 5;
+    protected override int NumberOfActions => 45;	
+}
